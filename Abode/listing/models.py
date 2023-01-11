b485eb1f9f4962cc7d3c2d4e06e1141d0d5071c3
0724654276 <you@example.com>
2023-01-11 16:51:21 +0300

    create model class for land

--- Abode/listing/models.py
+++ Abode/listing/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class land(models.Model):
+    location = models.CharField(max_length= "64", blank = False)
+    Size = models.IntegerField()
+    
